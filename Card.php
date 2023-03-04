e945280cb03019351dc667face8871ee10ba5c22
Yasmine <yasmine.amaddah@laplateforme.io>
2023-03-04 17:57:57 +0100

    update

--- Card.php
+++ Card.php
@@ -1,21 +1,14 @@
 <?php
-class Card {
 
-    // Les Attributs
-    public $image;
+class Card
+{
     private $id;
+    private $image;
     private $state;
-    private $items;
 
-    // public $etat;
-
-    // Le Constructeur
-    // public function __construct($idCard) {
-	// 	$ToutesLesImages = ['image/card1.jpg', 'image/card2.jpg', 'image/card3.jpg', 'image/card4.jpg', 'image/card5.jpg', 'image/card6.jpg', 'image/card7.jpg', 'image/card8.jpg', 'image/card9.jpg', 'image/card10.jpg', 'image/card11.jpg', 'image/card12.jpg'];
-	// 	$this->setImage($ToutesLesImages[$idCard]);
-	// }
-
-    // Les Methodes
+    /**
+     * @return mixed
+     */
     public function getId()
     {
         return $this->id;
@@ -60,5 +53,6 @@ class Card {
     {
         $this->state = $state;
     }
+
 }
 ?>
--- memory.php
+++ memory.php
@@ -1,22 +1,27 @@
-<?php session_start();
+<?php 
 require('card.php');
+session_start();
 
+// On instancie notre classe Card
 if (!isset($card)) {
     for ($i = 1; $i < 10; $i++) {
         $card[$i] = new Card();
+        $arrayCard[$i]= [];
         $card[$i]->setId($i);
         $card[$i]->setImage("image/" . $i . ".jpg");
-        // $card[$i]->setState("hide");
-        $card[$i+9] = new Card();
-        $card[$i+9]->setId($i+9);
-        $card[$i+9]->setImage("image/" . $i . ".jpg");
+        $card[$i]->setState("hide");
+
+        $cardId = $card[$i]->getId();
+        $cardImage = $card[$i]->getImage();
+        $cardState = $card[$i]->getState();
+        $arrayCard[$i] = ["id" => $cardId, "image"=> $cardImage, "state"=> $cardState];
     }
-    //shuffle($card);
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -24,48 +29,71 @@ if (!isset($card)) {
     <link rel="stylesheet" href="style.css" />
     <title>memory</title>
 </head>
+
 <body>
     <?php include("header.php"); ?>
     <main>
-        <table>
+        <table class="tab">
             <form method="get">
                 <!--Génération du tableau-->
                 <?php
+                 $duplicateArray = duplicateArray($arrayCard);
+                 $RA = randomArray($duplicateArray);
+                 $_SESSION['count'] = 0;
 
-                foreach ($card as $key => $value) {
-                    if (isset($_SESSION['card' . $key])) {
-                        $card[$key]->setState('visible');
-                        echo '<tr><input type="submit" name="' . 'card' . $key . 'visible"> <img height="100px" src="' . $card[$key]->getImage() . '">' . '</input></tr>';
-                    } else {
-                        if (isset($_GET['card' . $key])) {
-                            if ($_GET['card' . $key] == "Envoyer") {
-                                $_SESSION['card' . $key] = $_GET['card' . $key];
-                                //header('location:index');
+                if ($RA) {
+                    foreach ($RA as $key => $value) {
+
+                         if (isset($_SESSION['clickedImg']['card' . $key])) {
+                    echo '<tr><img height="100px" src="' . $value['image'] . '">' . '</tr>';
+                        } 
+                        else {
+                            if (isset($_GET['card' . $key])) {
+                                if ($_GET['card' . $key] == $value['id']) {
+                                    $_SESSION['clickedImg']['card' . $key] = $_GET['card' . $key];
+                                    $_SESSION['random'][$key]['state'] = "visible";
+                                    //header('location:index');
+                                }
+        
                             }
+                            echo '<tr><input type="submit" name="' . 'card' . $key . '" value ="'.$value['id'].'">  <img height="100px" src="image/back.jpg">' . '</input></tr>';
                         }
-                       echo '<tr><input type="submit" name="' . 'card' . $key . '"> <img height="100px" src="image/back.jpg">' . '</input></tr>';
                     }
                 }
                 ?>
                 <div>
-                <button class="button" type="submit" name="reset" value="reset">Reinitialiser la partie</button>
+                    <button class="button" type="submit" name="reset" value="reset">Reset la partie</button>
                 </div>
                 <?php
+
                 resetGame();
+                //        var_dump($_GET);
+                //
+                //var_dump($_SESSION);
+
+                function duplicateArray($array){
+                    $array2 = $array;
+                    $newArray = array_merge($array2,$array);
+                    return $newArray;
+                }
+                function randomArray($array){
+                    if(empty($_SESSION['random'])) {
+                        $randomArray = shuffle($array);
+                        $_SESSION['random'] = $array;
+                    }
+                    return $_SESSION['random'];
+                }
+        
                 function resetGame()
                 {
                     if (isset($_GET['reset'])) {
                         session_unset();
                         session_destroy();
                         unset($_GET);
-                        //header('location:index');
+                        header('location:index');
                     }
                 }
-                // var_dump($card);
-                // var_dump($_GET);
-                // //unset($_GET);
-                // //session_destroy();
-                // var_dump($_SESSION);
+
                 ?>
             </form>
         </table>
